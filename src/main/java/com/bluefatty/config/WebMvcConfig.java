4110dfdc191e2bcdc18bdd114000e7f6444627cb
PomZWJ <1513041820@qq.com>
2019-10-14 21:44:02 +0800

    web mvc配置

---
+++ src/main/java/com/bluefatty/config/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.bluefatty.config;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+/**
+ * web mvc配置
+ * @author PomZWJ
+ * @github https://github.com/PomZWJ
+ * @date 2019-10-14
+ */
+public class WebMvcConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
+    }
+}

d355a466f9f86bb5cf71efc711841bbda9914a1e
PomZWJ <zwj@19951123>
2019-10-15 15:12:51 +0800

    Token

---
+++ src/main/java/com/bluefatty/domain/Token.java
@@ -0,0 +1,29 @@
+package com.bluefatty.domain;
+
+/**
+ * Token
+ *
+ * @author PomZWJ
+ * @github https://github.com/PomZWJ
+ * @date 2019-10-15
+ */
+public class Token {
+    /**
+     * 签名
+     */
+    private String sign;
+    /**
+     * 用户实体
+     */
+    private User user;
+    /**
+     * 创建日期yyyyMMdd
+     */
+    private String createDate;
+    /**
+     * 创建时间HHmmss
+     */
+    private String createTime;
+
+
+}
